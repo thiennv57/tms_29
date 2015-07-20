851184db6ea8465483323c4e5537f7e9a1d4649f
Ngo Van Thien <thienbg93@gmail.com>
2015-07-20 09:41:39 +0700

    Edit user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,31 @@
 class UsersController < ApplicationController
-  before_action :init_user, only: [:show]
+  before_action :init_user, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update, :show]
+  before_action :authenticate_user!
+
+  def update
+    if @user.update user_params
+      sign_in @user, bypass: true
+      flash[:success] = t "messages.user.update.success"
+      redirect_to user_path @user
+    else
+      flash.now[:failed] = t "messages.user.update.fail"
+      render "edit"
+    end
+  end
 
   private
   def init_user
     @user = User.find params[:id]
+  end
+
+  def user_params
+    params.require(:user).permit :id, :name, :email,
+      :avatar, :password, :password_confirmation
+  end
+
+  def correct_user
+    @user = User.find params[:id]
+    redirect_to root_url unless current_user? @user
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,12 @@
 module UsersHelper
-  def gravatar_for user
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+  def gravatar_for user, options = {size: Settings.user.gravatar.image_size}
+    gravatar_id = Digest::MD5::hexdigest user.email.downcase
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag gravatar_url, alt: user.name, class: "gravatar"
+  end
+  
+  def current_user? user
+    current_user == user
   end
 end
