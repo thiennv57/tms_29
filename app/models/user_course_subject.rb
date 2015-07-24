71226d93a6a6bb33334bbb224035cbf0ea8edc46
Nguyen <nguyen.thanh.binh@framgia.com>
2015-07-24 13:11:05 +0700

    Finish subject-details

--- app/models/user_course_subject.rb
+++ app/models/user_course_subject.rb
@@ -3,4 +3,5 @@ class UserCourseSubject < ActiveRecord::Base
   belongs_to :subject
   has_many :user_course_subject_tasks, dependent: :destroy
   has_many :tasks, through: :user_course_subject_tasks
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,7 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:show, :edit, :update]
+  resources :courses do
+    resources :subjects, only: :show
+  end
 end
