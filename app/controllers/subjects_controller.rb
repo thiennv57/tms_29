709b3c9657eb773792fbb2b0d18c5c82268b5695
Ngo Van Thien <thienbg93@gmail.com>
2015-07-30 14:48:35 +0700

    Refactor course and subject show

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -2,7 +2,9 @@ class SubjectsController < ApplicationController
   def show
     user_course = current_user.user_courses.find_by course_id: params[:course_id]
     @user_course_subject = user_course.user_course_subjects.find_by subject_id: params[:id]
-    @subject = @user_course_subject.subject
+    unless @user_course_subject.nil?
+      @subject = @user_course_subject.subject
+    end
   end
 
   def update
