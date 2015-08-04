3465194b0baa79e80291fbc7162fe7888971fa99
Longvt <longvt94@gmail.com>
2015-08-04 16:14:54 +0700

    send supervisor mail about trainee report

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -16,4 +16,13 @@ class UserMailer < ApplicationMailer
     @course = course
     mail to: user.email, subject: I18n.t("mailer.unassigned")
   end
+  
+  def trainee_daily_report_infomation course
+    users = course.users
+    @activities = Activity.activities_by_user_ids(users.pluck :id)
+      .daily_report(Settings.activities.trainee_finish_task).recent.decorate
+    course.users.admins.each do |user|
+      mail to: user.email
+    end
+  end
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,4 +3,6 @@ class Activity < ActiveRecord::Base
 
   scope :recent, ->{order "created_at DESC"}
   scope :activities_by_user_ids, ->user_ids{where "user_id IN (?)", user_ids}
+  scope :daily_report, ->finish_task{where "date(created_at) = date(\"now\") 
+    and activity_type = ?", finish_task}
 end
